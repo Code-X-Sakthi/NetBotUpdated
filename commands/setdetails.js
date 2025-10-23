8a6ca7b6ad09bb1ec8b01c08244b87283bfdc51d
Sakthi Jena R <127412012+Code-X-Sakthi@users.noreply.github.com>
2025-10-23 19:14:31 +0530

    Update setdetails.js

--- commands/setdetails.js
+++ commands/setdetails.js
@@ -6,9 +6,9 @@
   folder: 
 CMD*/
 
-Bot.setProperty("admin_chat", "YOUR_TELEGRAM_ID", "string")
+Bot.setProperty("admin_chat", "5517793030", "string")
 Bot.setProperty("num", 0, "integer")
-Bot.setProperty("credentials", "YOUR_API_KEY_OR_SECRET", "string")
-Bot.setProperty("admin", "YOUR_TELEGRAM_USERNAME", "string")
+Bot.setProperty("credentials", "x123y45apikey", "string")
+Bot.setProperty("admin", "t.me/NovaxTG", "string")
 
 Bot.sendMessage("✅ Details saved successfully.")
